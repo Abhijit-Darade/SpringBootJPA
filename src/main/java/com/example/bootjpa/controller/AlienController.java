15b609177085eb66efadf18b6d7d15a5b1e883a4
abhijit20-vd <abhijitdarade.vd@gmail.com>
2024-06-26 16:25:06 +0530

    second

--- src/main/java/com/example/bootjpa/controller/AlienController.java
+++ src/main/java/com/example/bootjpa/controller/AlienController.java
@@ -1,15 +1,24 @@
 package com.example.bootjpa.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.bootjpa.dao.AlienRepo;
 import com.example.bootjpa.model.Alien;
 
-@Controller
+@RestController
 public class AlienController {
 
     @Autowired
@@ -20,20 +29,22 @@ public class AlienController {
         return "home.jsp";
     }
 
-    @RequestMapping("/addAlien")
-    public String addAlien(Alien alien){
+    @PostMapping("/alien")
+    public Alien addAlien(@RequestBody Alien alien){
         repo.save(alien);
-        return "home.jsp";
+        return alien;
     }
 
-    @RequestMapping("/getAlien")
-    public ModelAndView getAlien(@RequestParam int aid){
-        ModelAndView mv = new ModelAndView("showAlien.jsp");
-        Alien alien = repo.findById(aid).orElse(new Alien());
-        // System.out.println(repo.findByTech("Java"));
-        // System.out.println(repo.findByAidGreaterThan(102));
-        System.out.println(repo.findByTechSorted("Java"));
-        mv.addObject(alien);
-        return mv;
+    @GetMapping(path="/aliens")
+    public List<Alien> getAliens(){
+
+       return repo.findAll();
+        
+    }
+    @GetMapping("/alien/{aid}")
+    public Optional<Alien> getAlien(@PathVariable("aid") int aid){
+
+       return repo.findById(aid);
+        
     }
 }
--- src/main/java/com/example/bootjpa/dao/AlienRepo.java
+++ src/main/java/com/example/bootjpa/dao/AlienRepo.java
@@ -4,14 +4,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.bootjpa.model.Alien;
+
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AlienRepo extends CrudRepository<Alien, Integer> {
-    List<Alien> findByTech(String tech);
-    List<Alien> findByAidGreaterThan(int aid);
 
-    @Query("from Alien where tech=?1 order by aname desc")
-
-    List<Alien> findByTechSorted(String tech);
+public interface AlienRepo extends JpaRepository<Alien, Integer> {
+    
 }
